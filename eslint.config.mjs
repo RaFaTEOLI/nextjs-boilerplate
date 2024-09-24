65e47baa764b80e3665b3b82744d4e948cfcd3d2
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-09-24 14:24:55 -0300

    chore: add eslint-plugin-react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,12 +2,19 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import pluginReact from "eslint-plugin-react";
-
+import reactHooks from "eslint-plugin-react-hooks";
 
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
+  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
+  { languageOptions: { globals: globals.browser } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  reactHooks.configs.recommended,
+  {
+    rules: {
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+    },
+  },
 ];
