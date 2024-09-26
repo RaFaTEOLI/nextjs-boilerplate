47d1b5f47d0f42914e4c3818bd3017697370e69e
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-09-26 12:43:38 -0300

    feat: add home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,15 @@ import * as S from '@/pages/index.styles';
 export default function Home() {
   return (
     <S.Wrapper>
+      <S.Logo src="/img/logo.png" alt="Logo Next.js" />
       <S.Title>Next.js Boilerplate</S.Title>
+      <S.Description>
+        TypeScript, ReactJS, NextJS and Styled Components
+      </S.Description>
+      <S.Illustration
+        src="/img/coding.svg"
+        alt="A developer using a computer."
+      />
     </S.Wrapper>
   );
 }
