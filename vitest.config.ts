749fa448f045ff451fe85ff2eb4b76c3fa042105
RaFaTEOLI <rafinha.tessarolo@hotmail.com>
2024-09-26 12:34:24 -0300

    chore: setup path resolve with vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vitest/config';
+import path from 'path';
 
 export default defineConfig({
   test: {
+    globals: true,
     environment: 'jsdom',
     coverage: {
       include: ['src/**/*.ts(x)'],
@@ -15,5 +17,10 @@ export default defineConfig({
       }
     },
     setupFiles: ['./.vitest/setup.ts']
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
   }
 });
